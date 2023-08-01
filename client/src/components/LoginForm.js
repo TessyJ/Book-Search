00d6f1cb61255fac2fcf61aaad37fc2abd0f63cc
Theresa <theresaolusoga@Bolajis-MacBook-Pro.local>
2023-08-01 18:49:37 -0400

    loginform fixed

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,7 +2,6 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
 
 import { useMutation } from '@apollo/client';
 
